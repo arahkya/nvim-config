437eb3fb012bdf1258f802424425d284399a4a7a
Arahk Yambupah <2521092326@bangkokbank.com>
2024-08-13 21:07:48 +0700

    Add Go and Tree plugins

--- lua/keymap-config.lua
+++ lua/keymap-config.lua
@@ -1,2 +1,8 @@
 local builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<c-f>',builtin.find_files, {})
+
+vim.keymap.set('n', '<c-t>',":Neotree filesystem reveal left<CR>", {})
+vim.keymap.set('n', '<s-t>',":Neotree close<CR>", {})
+
+vim.keymap.set('n', '<c-b>',':GoDebugBreakpoint<CR>', {})
